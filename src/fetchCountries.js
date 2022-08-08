3cbbf0c6ca49b5ec60b2e963475804fa79fd3bdc
Dmytro Zhuk <dz@rengydevelopment.com.ua>
2022-08-08 14:20:37 +0300

    fixxx

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,22 +1,21 @@
 'use strict';
 
 export class RestCountriesApi {
-  #BASE_URL = 'https://restcountries.com/v3.1/name/';
+    #BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-  constructor() {
-    this.query = '';
-  }
+    constructor() {
+        this.query = '';
+    }
 
-  fetchCountries() {
-    return fetch(
-      `${this.#BASE_URL}${
-        this.query
-      }?fields=name,capital,population,languages,flags`
-    ).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-  }
+    fetchCountries() {
+        return fetch(
+            `${this.#BASE_URL}${this.query
+            }?fields=name,capital,population,languages,flags`
+        ).then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
+    }
 }
